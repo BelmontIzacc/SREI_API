a349887b037ddaf6d338ebb5734aa5eb9d12ef59
Belmont <iibarrab.dacompsc@gmail.com>
2020-08-29 22:49:03 -0500

    eliminar keys de acceso a firebase

--- src/environments/cert/cer-dev.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyBQj50PaQpXQUAdznzyXLI7ljlwqlCjFYg",
-    authDomain: "srei-dc583.firebaseapp.com",
-    databaseURL: "https://srei-dc583.firebaseio.com",
-    projectId: "srei-dc583",
-    storageBucket: "srei-dc583.appspot.com",
-    messagingSenderId: "1049002149570",
-    appId: "1:1049002149570:web:b5c5204157a36a80141d5b"
-}
